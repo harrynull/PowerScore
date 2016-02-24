f0a3845884d8c3372896294e01a79878bcc9eea9
Null <abc612008@126.com>
2016-02-24 20:39:10 +0800

    多班级支持（仍有Bug）

---
+++ app/src/main/java/com/github/hitgif/powerscore/Class.java
@@ -0,0 +1,20 @@
+package com.github.hitgif.powerscore;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Null on 2016/2/24.
+ */
+public class Class {
+    public Class(String _name, String _members){
+        histories = new ArrayList<History>();
+        scores = new ArrayList<Integer>();
+        name=_name;
+        members=_members.split(" ");
+    }
+
+    public String name;
+    public String[] members;
+    public ArrayList<History> histories = new ArrayList<History>();
+    public ArrayList<Integer> scores = new ArrayList<Integer>();
+}
