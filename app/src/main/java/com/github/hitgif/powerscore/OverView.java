da74dccaa2fa8173ffbb4d82abd4a4dc7a20cd8e
HITGIF <carbonylgroup@outlook.com>
2016-03-14 20:02:44 +0800

    0314

--- app/src/main/java/com/github/hitgif/powerscore/OverView.java
+++ app/src/main/java/com/github/hitgif/powerscore/OverView.java
@@ -1,8 +1,10 @@
 package com.github.hitgif.powerscore;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ExpandableListActivity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -15,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.CheckBox;
@@ -23,6 +26,7 @@ import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -71,26 +75,97 @@ public class OverView extends Activity {
 
         lv.setAdapter(new ExpandableAdapter(this));
 
+        lv.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition,
+                                        final int childPosition, long id) {
+                Classes c = null;
+                String Key="";
+                int cid = 0;
+                for (final String key : MainActivity.classes.keySet()) { //查找每个班
+                    if (cid == groupPosition) {
+                        c = MainActivity.classes.get(key);
+                        Key=key;
+                        break;
+                    }
+                    cid++;
+                }
+                final String fKey=Key;
+                if(c==null) return false;
+                new ActionSheetDialog(OverView.this).builder()
+                        .setTitle(String.valueOf(c.members[childPosition]))
+                        .setCancelable(false)
+                        .setCanceledOnTouchOutside(true)
+                        .addSheetItem("修改分数", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+                                        final EditText text = new EditText(OverView.this);
+                                        text.setText(String.valueOf(MainActivity.classes.get(fKey).scores[childPosition]/10.0));
+                                        new AlertDialog.Builder(OverView.this)
+                                                .setTitle("请设置分数")
+                                                .setIcon(android.R.drawable.ic_dialog_info)
+                                                .setView(text)
+                                                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        Classes c = MainActivity.classes.get(fKey);
+                                                        try {
+                                                            c.scores[childPosition]= (int) (Double.parseDouble(text.getText().toString()) * 10);
+                                                            showToast("设置分数成功");
+                                                        }catch (Exception e){
+                                                            showToast("设置分数失败:必须输入数字");
+                                                            e.printStackTrace();
+                                                        }
+                                                        ((BaseAdapter)lv.getAdapter()).notifyDataSetChanged();
+                                                    }})
+                                                .setNegativeButton("取消", null)
+                                                .show();
+                                    }
+                                })
+                        .addSheetItem("查看记录", ActionSheetDialog.SheetItemColor.Blue,
+                                new ActionSheetDialog.OnSheetItemClickListener() {
+                                    @Override
+                                    public void onClick(int which) {
+
+                                    }
+                                })
+                        .show();
+                return true;
+            }
+        });
+
         ((EditText) (findViewById(R.id.editText))).setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                filter=((EditText) (findViewById(R.id.editText))).getText().toString();
+                filter = ((EditText) (findViewById(R.id.editText))).getText().toString();
 
                 groupArray = new ArrayList<String>();
                 childArray = new ArrayList<List<String>>();
 
+                ArrayList<Integer> needExpand = new ArrayList<Integer>();
+
                 for (final String key : MainActivity.classes.keySet()) {
                     Classes c = MainActivity.classes.get(key);
-                    groupArray.add(c.name);
+
                     List<String> tempArray = new ArrayList<String>();
                     for (int j = 0; j < c.members.length; j++) {
-                        if(!filter.isEmpty()&&!c.members[j].contains(filter)) continue;
-                        tempArray.add(c.members[j]+"|"+c.scores[j]);
+                        if (!filter.isEmpty() && !c.members[j].contains(filter)) continue;
+                        tempArray.add(c.members[j] + "|" + c.scores[j]);
                     }
-                    childArray.add(tempArray);
+                    if (!tempArray.isEmpty()) {
+                        groupArray.add(c.name);
+                        childArray.add(tempArray);
+                        needExpand.add(groupArray.size() - 1);
+                    }
                 }
 
-                ((BaseAdapter)lv.getAdapter()).notifyDataSetChanged();
+                lv.setAdapter(new ExpandableAdapter(OverView.this));
+
+                for (int id : needExpand) {
+                    lv.expandGroup(id);
+                }
+
                 return false;
             }
         });
@@ -168,6 +243,9 @@ public class OverView extends Activity {
         {
             return  true ;
         }
+    }
 
+    private void showToast(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
