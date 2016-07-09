8bb2cc613a827a6b1dff9465cd19042076a6c93a
HITGIF <carbonylgroup@outlook.com>
2016-07-09 20:24:08 +0800

    添加组ui

---
+++ PowerScore/ChooseGroupMemberViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChooseGroupMemberViewController.h
+//  PowerScore
+//
+//  Created by Carbonylgroup on 7/2/16.
+//  Copyright © 2016 Gustav Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChooseGroupMemberViewController : UIViewController
+
+@end
